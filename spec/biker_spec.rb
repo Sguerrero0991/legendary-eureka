30b5ced36d82b5f102f3533b4883355aa09a868b
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-10 10:34:35 -0500

    Will add attributes to Biker class

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe Biker do
     expect(biker).to be_a(Biker)
   end
 
-
+  it 'has attributes' do
+    biker = Biker.new("Kenny", 30)
+    expect(biker.name).to eq("Kenny")
+    expect(biker.max_distance).to eq(30)
+  end
 end 
