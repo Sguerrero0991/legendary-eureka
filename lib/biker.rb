614daba3a38811bd5e0ce60cfe2c7d4c2bdebde9
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-10 10:35:55 -0500

    Adds additional attributes

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,9 +1,11 @@
 require 'pry'
 class Biker
-  attr_reader :name, :max_distance
+  attr_reader :name, :max_distance, :rides, :acceptable_terrain
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
   end
 
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe Biker do
     biker = Biker.new("Kenny", 30)
     expect(biker.name).to eq("Kenny")
     expect(biker.max_distance).to eq(30)
+    expect(biker.rides).to eq({})
   end
 end 
