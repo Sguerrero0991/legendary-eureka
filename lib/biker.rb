3d946bd4772a04e0138a94fcdb0963f90469cd12
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-10 10:33:33 -0500

    Will create Biker class and instatiate a biker object

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,5 +1,9 @@
 require 'pry'
-
 class Biker
+  attr_reader :name, :max_distance
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+  end
 
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -1,2 +1,12 @@
 require './lib/ride'
+require './lib/biker'
 require 'pry'
+
+RSpec.describe Biker do
+  it 'exists' do
+    biker = Biker.new("Kenny", 30)
+    expect(biker).to be_a(Biker)
+  end
+
+
+end 
